90a0449ea54fd3ea26f7037706c76834705ae312
Dorian LECOMTE <dorian.lecomte@viacesi.fr>
2026-03-16 13:56:08 +0100

    Modal Ajouter

---
+++ Projet_web_Talia/src/Application/Controller/OffresController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Application\Controller;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Views\Twig;
+
+
+class OffresController
+{ public function ajouter(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        $view = Twig::fromRequest($request);
+        return $view->render($response, 'Offres/Page_Modal_Ajout_Offre.html.twig', []);
+    }
+}
