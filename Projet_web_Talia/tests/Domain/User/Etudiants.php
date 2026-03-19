db947b057b8a8972b916efc576b76d71dec2dcc8
Evariste Bride <evariste.bride@viacesi.fr>
2026-03-19 17:32:06 +0100

    Fichier tableau php sans erreur

--- Projet_web_Talia/tests/Domain/User/Etudiants.php
+++ Projet_web_Talia/tests/Domain/User/Etudiants.php
@@ -7,10 +7,10 @@ use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\Table;
 
-#[Entity, Table(name: 'Etudiants')] // <--- Le nom exact de ta table dans phpMyAdmin
+#[Entity, Table(name: 'Etudiants')]
 class Etudiant
 {
-    #[Id, Column(type: 'integer'), GeneratedValue(strategy: 'AUTO')]
+    #[Id, Column(type: 'int')]
     private int $id;
 
     #[Column(type: 'string')]
@@ -22,7 +22,12 @@ class Etudiant
     #[Column(type: 'string')]
     private string $email;
 
-    // Getters et Setters (indispensables pour manipuler les données)
+    #[Column(type: 'string')]
+    private string $campus;
+
+    #[Column(type: 'string')]
+    private string $ville;
+
     public function getId(): int { return $this->id; }
     
     public function setPrenom(string $prenom): void { $this->prenom = $prenom; }
@@ -33,4 +38,10 @@ class Etudiant
 
     public function setEmail(string $email): void { $this->email = $email; }
     public function getEmail(): string { return $this->email; }
+
+    public function setCampus(string $email): void { $this->email = $email; }
+    public function getCampus(): string { return $this->email; }
+
+    public function setVille(string $email): void { $this->email = $email; }
+    public function getVille(): string { return $this->email; }
 }
