05eabb711b8c11f7b93db8825d3d3c1d36fc382a
Evariste Bride <evariste.bride@viacesi.fr>
2026-03-23 18:42:51 +0100

    prout

--- Projet_web_Talia/app/routes.php
+++ Projet_web_Talia/app/routes.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\App;
+use Slim\App;   
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 use App\Application\Controller\HomeController;
 use App\Application\Controller\EntrepriseController;
